561da06dca1ea3a0b175a65d65ab68264b2ccbf3
Rodrigo Correa de Godoy Camargo <rodrigo.godoy@ambevtech.com.br>
2021-05-31 19:30:09 -0300

    first commit

---
+++ dtmoney/src/App.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+
+
+const Title = styled.h1`
+color : #8257e6 
+
+
+`
+
+export function App() {
+  return (
+    <div className="App">
+      <Title>hello world</Title>
+    </div>
+  );
+}
+
+
